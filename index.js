f4e26dd45acd7cb5f47ff0dfd50f822636810d88
Gina Henderson <gina@ginahenderson.me>
2022-09-25 14:59:23 -0500

    Create label for list

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-exports.print = () => console.log('hello!!!1!')
+export const dropdown = () => {
+  const dropdowns = [...document.querySelectorAll('[data-dropdown]')];
+  const style = `
+        position:absolute;
+        list-style:none;
+        top:100%;
+        width: 100%;
+        border:solid 1px green;
+    `;
+  dropdowns.forEach((a) => {
+    const wrapper = document.createElement('div');
+    a.parentElement.append(wrapper);
+    const label = document.createElement('span');
+    wrapper.append(label, a);
+    wrapper.style = `
+        position:relative;
+        display:inline-block;
+        border:solid 1px red
+        `;
+    const labelInfo = (() => {
+      const obj = {};
+      const items = a.dataset.label.split(',').map((a) => {
+        
+        a = a.trim();
+      });
+      console.log(items);
+    })();
+    label.textContent = a.dataset.dropdown;
+    a.parentElement.insertBefore(label, a);
+    label.classList.add('dropdown-label');
+    label.style.display = 'inline-block';
+    a.style = style;
+  });
+};
